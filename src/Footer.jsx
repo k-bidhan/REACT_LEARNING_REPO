f910552a5fdd202bf84c02ea9a3c94ac5f13bdaa
Your Name <your.email@example.com>
2025-05-19 14:36:36 +0545

    Fresh commit to new GitHub repo

---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy;{new Date().getFullYear()} My React Site</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+
+function Header(){
+    return(
+        <header>
+      <h1>My React Site</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Products</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
+      <hr />
+    </header>
+    )
+}
+
+export default Header 
